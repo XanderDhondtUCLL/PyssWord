37135b2ffadb6250d5fb55fa58bce76d80de57e5
XanderDhondtUCLL <xander.dhondt@student.ucll.be>
2024-03-09 02:10:02 +0100

    first character generation

--- src/Pasgenerator.py
+++ src/Pasgenerator.py
@@ -1,6 +1,10 @@
+import string
+import secrets
+
 class Pasgenerator:
     def __init__(self):
         self.__length = 12
+        self.__pasword = ''
     
     @property
     def length(self):
@@ -10,4 +14,17 @@ class Pasgenerator:
     def length(self, value: int):
         self.__length = value
 
+    @property
+    def password(self):
+        return self.__pasword
+
+    def generate(self):
+        # TODO
+        # [x] generation of first character
+        # [ ] logic to decide which next character of password will be.
+
+        # generate the first character of the password.
+        self.__pasword = ''.join(secrets.choice(string.ascii_letters + string.digits)) 
+        for i in range(self.__length):
+            pass
     
